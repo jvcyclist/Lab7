0a7c274963b33173c976dfd31d7040bafde15514
Patryk <patryk1karas@gmail.com>
2018-02-15 22:22:29 +0100

    Update2

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,5 @@
 public class HelloWorld{
-
-public static void main()
-{System.out.println("Hello World for lab 7");
-
-
-}
+  public static void main(String[] args){
+    System.out.print("Hello World for lab seven");
+  }
 }
